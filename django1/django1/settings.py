b16df64e46af1dc816dde5084b456df05b2f25e6
Brighten <brighten@Brightens-MacBook-Pro.local>
2019-11-07 11:30:40 +0900

    test

--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'asdfasdf'
+SECRET_KEY = 'test_secretKey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dsgsadfd']
+ALLOWED_HOSTS = ['host']
 
 
 # Application definition
